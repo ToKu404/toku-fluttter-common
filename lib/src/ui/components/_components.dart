34b1f667f45516182aeeb16462a61496a0c036ea
toku404 <phantom26isn@gmail.com>
2026-05-10 19:40:08 +0800

    Remove BrutalismButton and BrutalismCard exports from components

--- lib/src/ui/components/_components.dart
+++ lib/src/ui/components/_components.dart
@@ -1,6 +1,4 @@
 export 'advanced/_advanced.dart';
 export 'animated_overlay/animated_overlay.dart';
-export 'brutalism/brutalism_button.dart';
-export 'brutalism/brutalism_card.dart';
 export 'infinity_scroll_builder.dart';
 export 'slivers/sliver_sized_box.dart';
